e85c692485198baca3a048a954190dbb5704de81
patrickreck <reckpatrick@gmail.com>
2019-05-12 09:22:18 +0200

    add line bug

--- App.js
+++ App.js
@@ -1,21 +1,36 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {Component} from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import {createAppContainer, createDrawerNavigator} from "react-navigation";
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
-  }
+class HomeScreen extends Component {
+  render = () => <View style={styles.home}><Text style={styles.homeText}>Drag me --></Text></View>
 }
 
+export default createAppContainer(
+  createDrawerNavigator({
+      HomeScreen: HomeScreen,
+    },
+    {
+      drawerType: 'slide',
+      overlayColor: 'transparent',
+      style: {backgroundColor: 'purple'},
+      edgeWidth: 200,
+    },
+    {
+      initialRouteName: 'HomeScreen',
+    })
+);
+
 const styles = StyleSheet.create({
-  container: {
+  home: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: 'purple',
+    alignSelf: 'stretch',
     justifyContent: 'center',
+    alignContent: 'center',
   },
+  homeText: {
+    color: 'white',
+    marginLeft: 10,
+  }
 });
